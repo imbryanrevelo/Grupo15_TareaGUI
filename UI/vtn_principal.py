be7cde925403d9c305c79154ea8e05ee0490ade1
Bryan Revelo <bryanreveloyagual@outlook.com>
2023-07-19 16:32:10 -0500

    Add files via upload

---
+++ UI/vtn_principal.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'vtn_principal.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QLabel, QLineEdit,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
+
+class Ui_vtn_principal(object):
+    def setupUi(self, vtn_principal):
+        if not vtn_principal.objectName():
+            vtn_principal.setObjectName(u"vtn_principal")
+        vtn_principal.resize(320, 243)
+        self.centralwidget = QWidget(vtn_principal)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.txt_nombre = QLineEdit(self.centralwidget)
+        self.txt_nombre.setObjectName(u"txt_nombre")
+        self.txt_nombre.setGeometry(QRect(110, 50, 131, 31))
+        self.txt_apellido = QLineEdit(self.centralwidget)
+        self.txt_apellido.setObjectName(u"txt_apellido")
+        self.txt_apellido.setGeometry(QRect(110, 90, 131, 31))
+        self.lbl_nombre = QLabel(self.centralwidget)
+        self.lbl_nombre.setObjectName(u"lbl_nombre")
+        self.lbl_nombre.setGeometry(QRect(50, 60, 51, 16))
+        self.lbl_apellido = QLabel(self.centralwidget)
+        self.lbl_apellido.setObjectName(u"lbl_apellido")
+        self.lbl_apellido.setGeometry(QRect(50, 100, 51, 16))
+        self.btn_grabar = QPushButton(self.centralwidget)
+        self.btn_grabar.setObjectName(u"btn_grabar")
+        self.btn_grabar.setGeometry(QRect(120, 160, 75, 23))
+        self.cb_tipo_persona = QComboBox(self.centralwidget)
+        self.cb_tipo_persona.addItem("")
+        self.cb_tipo_persona.addItem("")
+        self.cb_tipo_persona.setObjectName(u"cb_tipo_persona")
+        self.cb_tipo_persona.setGeometry(QRect(110, 20, 131, 22))
+        self.lbl_tipo_persona = QLabel(self.centralwidget)
+        self.lbl_tipo_persona.setObjectName(u"lbl_tipo_persona")
+        self.lbl_tipo_persona.setGeometry(QRect(50, 20, 51, 16))
+        vtn_principal.setCentralWidget(self.centralwidget)
+        self.mnb_menu_principal = QMenuBar(vtn_principal)
+        self.mnb_menu_principal.setObjectName(u"mnb_menu_principal")
+        self.mnb_menu_principal.setGeometry(QRect(0, 0, 320, 21))
+        vtn_principal.setMenuBar(self.mnb_menu_principal)
+        self.stb_estado = QStatusBar(vtn_principal)
+        self.stb_estado.setObjectName(u"stb_estado")
+        vtn_principal.setStatusBar(self.stb_estado)
+
+        self.retranslateUi(vtn_principal)
+
+        QMetaObject.connectSlotsByName(vtn_principal)
+    # setupUi
+
+    def retranslateUi(self, vtn_principal):
+        vtn_principal.setWindowTitle(QCoreApplication.translate("vtn_principal", u"Ventana Principal", None))
+        self.lbl_nombre.setText(QCoreApplication.translate("vtn_principal", u"Nombre:", None))
+        self.lbl_apellido.setText(QCoreApplication.translate("vtn_principal", u"Apellido:", None))
+        self.btn_grabar.setText(QCoreApplication.translate("vtn_principal", u"Grabar", None))
+        self.cb_tipo_persona.setItemText(0, QCoreApplication.translate("vtn_principal", u"Docente", None))
+        self.cb_tipo_persona.setItemText(1, QCoreApplication.translate("vtn_principal", u"Estudiante", None))
+
+        self.lbl_tipo_persona.setText(QCoreApplication.translate("vtn_principal", u"Tipo", None))
+    # retranslateUi
+
